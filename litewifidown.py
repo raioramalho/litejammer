6a018d018f5c773c775c1aa02a1168aeb076b491
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-19 18:36:13 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -3,12 +3,18 @@ import os
 import sys
 import string
 
+
+print("Check dependencies: ")
+print("")
+
 os.system('test /usr/bin/python  && echo "[+] Python ok" || apt-get -y install python')
 os.system('test /usr/bin/mdk3 && echo "[+] Mdk3 ok" || apt-get -y install mdk3')
 os.system('test /usr/bin/aircrack-ng && echo "[+] Aircrack-ng ok" || apt-get -y install aircrack-ng')
 print("")
 
 print("Listing Wireless extensions: ")
-os.system("ifconfig | grep -e ': ' | sed -e 's/: .*//g' | sed -e 's/^/   /' | grep 'w' > wlancards.txt && cat -n wlancards.txt")
+os.system("ifconfig | grep -e ': ' | sed -e 's/: .*//g' | sed -e 's/^//' | grep 'w' > card.txt && cat -n card.txt")
 print("")
+
 wlancard = raw_input("Please select one Wireless extension: ")
+os.system('tail -n +'+wlancard+' cards.txt')
