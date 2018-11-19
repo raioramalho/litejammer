09ea44ec62f421a2e205588a5ebab0e9720af050
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-19 18:11:38 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -8,5 +8,5 @@ os.system('test /usr/bin/mdk3 && echo "[+] Mdk3 ok" || apt-get -y install mdk3')
 os.system('test /usr/bin/aircrack-ng && echo "[+] Aircrack-ng ok" || apt-get -y install aircrack-ng')
 
 print("Listing Wlancard: ")
-wlanscan = os.system('ifconfig | grep -e ": " | sed -e 's/: .*//g' | sed -e 's/^/   /' | grep "w"')
+wlanscan = os.system("ifconfig | grep -e ": " | sed -e 's/: .*//g' | sed -e 's/^/   /' | grep 'w'")
 wlancard = raw_input("Please select one Wlancard: ")
