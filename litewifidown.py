0a95b8de62280146fac5c8ff6ac7c1410d24f92d
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-19 21:32:09 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -37,7 +37,7 @@ print("")
 
 print("Detecting Wireless Networks: ")
 os.system('card=`cat card.sh` && nmcli device wifi rescan ifname $card')
-os.system('card=`cat card.sh` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SECURITY,SSID, dev wifi ifname $card')
+os.system('card=`cat card.sh` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SECURITY,SSID, dev wifi list ifname $card')
 print("")
 
 choose=raw_input("Choose the wifi to attack: ")
