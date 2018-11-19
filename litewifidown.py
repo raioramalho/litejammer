9eb8fa22a0c4cba36369a8a38e10f5c48ee7cc7e
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-19 17:52:17 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -1,1 +1,8 @@
 #(start)#
+import os
+import sys
+import string
+
+os.system('test /usr/bin/python  && echo "[+] Python ok" || apt-get -y install python')
+os.system('test /usr/bin/mdk3 && echo "[+] Mdk3 ok" || apt-get -y install mdk3')
+os.system('test /usr/bin/aircrack-ng && echo "[+] Aircrack-ng ok" || apt-get -y install aircrack-ng')
