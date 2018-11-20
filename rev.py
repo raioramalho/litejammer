62cd9049a86642c46c146cb11d6262d1644fdd0c
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-20 15:08:42 -0200

    Update rev.py

--- rev.py
+++ rev.py
@@ -22,9 +22,31 @@ logo = ("""
    ||O@`=='@O||     | || |            !!             -air      Attack with the aireplay-ng     !! 
    ||_@\/\/@_||     |_||_|            !!             -mdk      Attack with the mdk3            !! 
  ----------------   '_'`_`            !!      Options required:                                !! 
-/________________\----------\         !!             -i        Set Interface name              !! 
+/________________\----------\         !!             -i        Set Interface name (-air)(-mdk) !! 
 |   GUILLOTINE   |-----------|        !!             -b        Set target BSSID for (-air)     !! 
 |  OF WIRELESS NETWORKS      |        !!             -c        Set channel for monitor (-air)  !!  
 |____________________________|        !!             -e        Set target ESSID for (-mdk)     !!   
                                       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 """)
+
+def cls():
+   os.system('clear')
+
+
+
+
+def start():
+   cls()
+   print(logo)
+   
+   if(sys.argv[1] == "-air"):
+      print("aireplay-ng attack")
+      
+      
+   if(sys.argv[1] == "-mdk"):
+      print("mdk3 attack")
+      
+try:
+   sys.argv[1]
+except Exception:
+   print("normal attack")
