1e57059d30e014b60722fbb75d9a263929294bd8
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-19 22:49:55 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -47,7 +47,8 @@ print("")
 
 choose=int(raw_input("Choose the wifi to attack: "))
 choose= choose + 1
-os.system('echo "'+str(choose)+'" > choose')
+choose=str(choose)
+os.system('echo "'+choose+'" > choose')
 os.system('echo "p" > p')
 os.system('card=`cat card.sh` && p=`cat p` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname wlan1 | awk '{print FS3 $3}' | sed '$choose$p; d'')
 os.system('card=`cat card.sh` && ch=`cat chan` && airmon-ng stop $card && airmon-ng start $card $ch')
