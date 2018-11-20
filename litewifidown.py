a87f11161d7abf6e9ac311167d5d0bf4deeeed38
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-20 01:19:43 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -72,4 +72,5 @@ else:
 print("")  
 print("")
 print("Return configurations: ")
+os.system('rm choose && rm chan && rm card.sh && rm targ')
 os.system('card=`cat card.sh` && ifconfig $card down && macchanger -p $card && ifconfig $card up')
