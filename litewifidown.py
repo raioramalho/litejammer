8528a45eccb8701fd59456c44d639a80cac0035f
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-20 03:09:14 -0200

    Update litewifidown.py

--- litewifidown.py
+++ litewifidown.py
@@ -29,7 +29,7 @@ print("")
 os.system(cls)
 
 print("Listing Wireless cards: ")
-os.system('test /monitor.rmo && rm monitor.rmo || ifconfig | grep -e ': ' | sed -e 's/: .*//g' | sed -e 's/^//' > monitor.rmo')
+os.system("test /monitor.rmo && rm monitor.rmo || ifconfig | grep -e ': ' | sed -e 's/: .*//g' | sed -e 's/^//' > monitor.rmo")
 monitor = raw_input("Select you wirelles card fo monitor mode:")
 os.system('cat monitor.rmo | grep -n ^ | grep '+monitor+' | cut -d: -f2 > monitor.rmo')
 os.system('cat monitor.rmo')
